0b68b5053bd72be3a42e4b35e87f20c9fc18caa8
jrthartwig <jocelynnhartwig@gmail.com>
2022-02-09 19:38:00 -0500

    feat: fixing www to run dev version

--- src/App.js
+++ src/App.js
@@ -15,19 +15,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <h2>Hooray Articles</h2>
         <h3>Hooray HMR</h3>
+
+
+
+
+
 
         {articles && (
           <ul>
